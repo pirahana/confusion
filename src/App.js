76068359e73537920fa39809054e5445db238fc8
makrand.sethi <makrand.sethi@ril.com>
2018-12-01 19:34:36 +0530

    Components part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    debugger;
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
+    debugger;
     return (
       <div className="App">
       <Navbar dark color="primary">
@@ -13,6 +23,7 @@ class App extends Component {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu dishes={this.state.dishes} />
     </div>
     );
   }
